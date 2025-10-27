4b75b22e01a8a4e24f7182aa4de57141e1372f87
yg-bit <jamesisaiah164@gmail.com>
2025-10-27 12:39:07 +0300

    added

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,14 @@
-// Your tests here
+import { currentAgeForBirthYear } from "../utils";
+
+describe("currentAgeForBirthYear", () => {
+  it("returns the age of a person based on the year of birth", () => {
+    
+    const currentYear = new Date().getFullYear();
+    const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    expect(ageOfPerson).toEqual(answer);
+  });
+});
